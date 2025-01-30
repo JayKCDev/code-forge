9dfb136e8f2078a3cfddf0807b094aef9fa1c09c
JayKCDev <jay.kc.fullstackdeveloper@gmail.com>
2025-01-30 13:00:19 +0530

    style changes

--- src/app/(root)/_components/RunButton.tsx
+++ src/app/(root)/_components/RunButton.tsx
@@ -60,7 +60,7 @@ function RunButton() {
 						<div className="relative flex items-center justify-center w-4 h-4">
 							<Play className="w-4 h-4 text-white/90 transition-transform group-hover:scale-110 group-hover:text-white" />
 						</div>
-						<span className="text-sm font-medium text-white/90 group-hover:text-white">
+						<span className="text-sm font-medium text-white/90 group-hover:text-white cursor-pointer">
 							Run Code
 						</span>
 					</>
